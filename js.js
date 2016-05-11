2e4b3a634e89a6425f80076a2d99443d0257f324
seandoherty10 <sean.doherty@greenwichschools.org>
2016-05-11 10:31:51 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$("#submit").on('click',function(){
+var firstname = $("#firstname").val();
+var lastname = $("#lastname").val();
+console.log(firstname);
+console.log(lastname);
+});
+});
